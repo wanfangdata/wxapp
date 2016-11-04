8a729a98e41fec9d1a28459ac529d0917ae5c106
biss <biss@wanfangdata.com.cn>
2016-11-04 17:30:12 +0800

    添加检索页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,13 +3,13 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '万方数据微信小程序',
     userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../search/search'
     })
   },
   onLoad: function () {
---
+++ pages/search/search.js
@@ -0,0 +1,15 @@
+//logs.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+      iconColor: 'purple',
+    iconType:'search'
+  },
+  onReady: function(){
+
+  },
+  onLoad: function () {      
+    
+  }
+})
