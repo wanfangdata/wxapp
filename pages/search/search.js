2c99832ada756da169c8f87f2772992bf1e48097
biss <biss@wanfangdata.com.cn>
2016-11-07 15:07:08 +0800

    轮播图

--- pages/search/search.js
+++ pages/search/search.js
@@ -3,6 +3,16 @@ var util = require('../../utils/util.js')
 var testData = require('testData.js')
 Page({
   data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+
     paperList:[]
   },
   formSubmit: function(e) {
