ec996b7e9293a0206b01430c8528730ca785491f
biss <biss@wanfangdata.com.cn>
2016-11-08 17:04:19 +0800

    首页资源介绍显示

--- pages/index/data.js
+++ pages/index/data.js
@@ -8,7 +8,8 @@ module.exports= [
         update: 13928,
         updateDate: '2016-11-07',
         searchTip: '在{total}篇论文中检索',
-        desc: '《中国学术期刊数据库》（China Science Periodical Database，CSPD）,收录始于1998年,7600余种，核心刊3000种，年增300万篇，周更新2次,涵盖理、工、农、医、经济、教育、文艺、社科、哲学政法等学科,全部拥有国内统一连续出版物号,免费注册DOI。'
+        desc: '《中国学术期刊数据库》（China Science Periodical Database，CSPD）,收录始于1998年,7600余种，核心刊3000种，年增300万篇，周更新2次,涵盖理、工、农、医、经济、教育、文艺、社科、哲学政法等学科,全部拥有国内统一连续出版物号,免费注册DOI。',
+        unit:'篇'
     },{
         id:'xw',
         name: '学位',
@@ -16,7 +17,8 @@ module.exports= [
         update: 14561,
         updateDate: '2016-11-02',
         searchTip: '在{total}篇论文中检索',
-        desc: '中国学位论文全文数据库（China Dissertation Database，CDDB），收录始于1980年，年增30万篇，并逐年回溯，与国内900余所高校、科研院所合作，占研究生学位授予单位85%以上，涵盖理、工、农、医、人文社科、交通运输、航空航天、环境科学等各学科。'
+        desc: '中国学位论文全文数据库（China Dissertation Database，CDDB），收录始于1980年，年增30万篇，并逐年回溯，与国内900余所高校、科研院所合作，占研究生学位授予单位85%以上，涵盖理、工、农、医、人文社科、交通运输、航空航天、环境科学等各学科。',
+        unit:'篇'
     },{
         id:'hy',
         name: '会议',
@@ -24,7 +26,8 @@ module.exports= [
         update: 18550,
         updateDate: '2016-11-02',
         searchTip: '在{total}篇论文中检索',
-        desc: '中国学术会议文献数据库（China Conference Paper Database，CCPD），收录始于1983年，4000个重要的学术会议，年增20万篇全文，每月更新，国家级学会、协会、部委、高校召开的全国性学术会议为主，国内目前收录会议数量较多、质量较高、学科覆盖较广。'
+        desc: '中国学术会议文献数据库（China Conference Paper Database，CCPD），收录始于1983年，4000个重要的学术会议，年增20万篇全文，每月更新，国家级学会、协会、部委、高校召开的全国性学术会议为主，国内目前收录会议数量较多、质量较高、学科覆盖较广。',
+        unit:'篇'
     },{
         id:'nstl',
         name: '外文文献',
@@ -33,6 +36,7 @@ module.exports= [
 			updateDate: '2016-04-25',
 			searchTip: '在{total}篇论文中检索',
 			desc: '外文文献数据库（National Science and Technology library，NSTL），万方数据与NSTL合作，一站式统一检索及原文传递，外文期刊收录始于1995年，世界知名出版社20000余种学术期刊，年增百万余篇，外文会议始于1985年，世界各学协会、出版机构，30000册论文集，年增20余万篇。',
+            unit:'篇'
     },{
         id:'nstr',
         name: '科技报告',
@@ -40,7 +44,8 @@ module.exports= [
 			update: 7685,
 			updateDate: '2016-05-05',
 			searchTip: '在{total}份科技报告中检索',
-			desc: '中文科技报告，收录始于1966年，源于中华人民共和国科学技术部，20000余份，外文科技报告，收录始于1958年，美国政府四大科技报告（AD、DE、NASA、PB），1100000余份。'
+			desc: '中文科技报告，收录始于1966年，源于中华人民共和国科学技术部，20000余份，外文科技报告，收录始于1958年，美国政府四大科技报告（AD、DE、NASA、PB），1100000余份。',
+            unit:'篇'
     },{
         id:'patent',
         name: '专利',
@@ -48,7 +53,8 @@ module.exports= [
 			update: 792779,
 			updateDate: '2016-10-13',
 			searchTip: '在{total}件专利中检索',
-			desc: '中外专利数据库（Wanfang Patent Database，WFPD），收录始于1985年，4500余万项专利，年增25万条，11国：中国、美国、澳大利亚、加拿大、瑞士、德国、法国、英国、日本、韩国、俄罗斯；两组织为：世界专利组织、欧洲专利局。'
+			desc: '中外专利数据库（Wanfang Patent Database，WFPD），收录始于1985年，4500余万项专利，年增25万条，11国：中国、美国、澳大利亚、加拿大、瑞士、德国、法国、英国、日本、韩国、俄罗斯；两组织为：世界专利组织、欧洲专利局。',
+            unit:'件'
     },{
         id:'standard',
         name: '标准',
@@ -56,7 +62,8 @@ module.exports= [
 			update: 4717,
 			updateDate: '2016-07-04',
 			searchTip: '在{total}项标准中检索',
-			desc: '中外标准数据库（Wanfang Standards Database，WFSD），收录37万余条，全文数据来源于国家指定标准出版单位，专有出版，文摘数据来自中国标准化研究院国家标准馆，数据权威。'
+			desc: '中外标准数据库（Wanfang Standards Database，WFSD），收录37万余条，全文数据来源于国家指定标准出版单位，专有出版，文摘数据来自中国标准化研究院国家标准馆，数据权威。',
+            unit:'项'
     },{
         id:'localchronicleitem',
         name: '地方志',
@@ -64,7 +71,8 @@ module.exports= [
 			update: 897,
 			updateDate: '2015-07-06',
 			searchTip: '在{total}条条目中检索',
-			desc: '中国地方志数据库（China Local Gazetteers Database，CLGD），新方志收录始于1949年，40000余册，旧方志收录年代为0000-1949年，预计近50000册。'
+			desc: '中国地方志数据库（China Local Gazetteers Database，CLGD），新方志收录始于1949年，40000余册，旧方志收录年代为0000-1949年，预计近50000册。',
+            unit:'条'
     },{
         id:'cstad',
         name: '成果',
@@ -72,7 +80,8 @@ module.exports= [
 			update: 4101,
 			updateDate: '2016-10-11',
 			searchTip: '在{total}项成果中检索',
-			desc: '中国科技成果数据库（China Scientific & Technological Achievements Database，CSTAD），收录始于1978年，来源于各省、市、部委鉴定后上报国家，科技部的科技成果及星火科技成果，涵盖新技术、新产品、新工艺、新材料、新设计等众多学科领域。'
+			desc: '中国科技成果数据库（China Scientific & Technological Achievements Database，CSTAD），收录始于1978年，来源于各省、市、部委鉴定后上报国家，科技部的科技成果及星火科技成果，涵盖新技术、新产品、新工艺、新材料、新设计等众多学科领域。',
+            unit:'项'
     },{
         id:'claw',
         name: '法规',
@@ -81,6 +90,7 @@ module.exports= [
 			updateDate: '2016-10-25',
 			searchTip: '在{total}条法规中检索',
 			desc: '中国法律法规数据库（China Laws & Regulations Database，CLRD），收录始于1949年，数据源自国家信息中心，权威、专业，涵盖国家法律法规、行政法规、地方法规、国际条约及惯例、司法解释、合同范本等。',
+            unit:'条'
     },{
         id:'institution',
         name: '机构',
@@ -88,7 +98,8 @@ module.exports= [
 			update: 233,
 			updateDate: '2016-10-11',
 			searchTip: '在{total}个机构中检索',
-			desc: '中国机构数据库（China Institution Database，CIDB），中国企业、公司及产品数据库，国内企业信息，中国科研机构数据库，国内科研机构信息，中国科技信息机构数据库，我国科技信息、高校图情单位信息，中国中高等教育机构数据库，国内高校信息。'
+			desc: '中国机构数据库（China Institution Database，CIDB），中国企业、公司及产品数据库，国内企业信息，中国科研机构数据库，国内科研机构信息，中国科技信息机构数据库，我国科技信息、高校图情单位信息，中国中高等教育机构数据库，国内高校信息。',
+            unit:'个'
     },{
         id:'book',
         name: '图书',
@@ -96,7 +107,8 @@ module.exports= [
 			update: 1255,
 			updateDate: '2014-12-06',
 			searchTip: '在{total}本图书中检索',
-			desc: '中国特种图书数据库（China Special Books Database，CSBD），图书内容针对性强，来源权威 ，保持原书原貌，享受原汁原味的阅读乐趣。'
+			desc: '中国特种图书数据库（China Special Books Database，CSBD），图书内容针对性强，来源权威 ，保持原书原貌，享受原汁原味的阅读乐趣。',
+            unit:'本'
     },{
         id:'expert',
         name: '专家',
@@ -104,7 +116,8 @@ module.exports= [
 			update: 50,
 			updateDate: '2015-05-14',
 			searchTip: '在{total}位专家中检索',
-			desc: '中国科技专家库（China Experts & Scholar Database，CESD），收录两院院士、高校博导、高产作者及其他科技专家信息12000余条，国内理、工、农、医、人文社科等领域的专家名人信息。'
+			desc: '中国科技专家库（China Experts & Scholar Database，CESD），收录两院院士、高校博导、高产作者及其他科技专家信息12000余条，国内理、工、农、医、人文社科等领域的专家名人信息。',
+            unit:'位'
     },{
         id:'scholar',
         name: '学者',
@@ -112,6 +125,7 @@ module.exports= [
 			update: 1755808,
 			updateDate: '2016-08-05',
 			searchTip: '在{total}位学者中检索',
-			desc: '信息来自万方学术圈，可获取学者的新情况、新研究领域、分享学术成果。'
+			desc: '信息来自万方学术圈，可获取学者的新情况、新研究领域、分享学术成果。',
+            unit:'位'
     }
 ];
